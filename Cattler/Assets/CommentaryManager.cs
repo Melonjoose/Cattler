0b57412fc82cd1f9c7a8556364e3a060fa6578f3
#SYLVESTIAN LEE YONG KANG# <SYLV0022@student.main.ntu.edu.sg>
2025-10-02 20:02:47 +0800

    v0.21.1
    Learnt about
    Inheritence
    virtual → base class says “you may override me.”
    override → child class says “I am replacing the base version.”
    new → child hides the parent’s method, no polymorphism.
    abstract → child must implement this method, no default provided.
    
    - Commentary Manager Added
     - Ability for Chat box to work!
    
    > Need to work on the triggers (low hp cat)**

--- Cattler/Assets/CommentaryManager.cs
+++ Cattler/Assets/CommentaryManager.cs
@@ -1,16 +1,72 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class CommentaryManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static CommentaryManager instance;
+    public TextMeshProUGUI text;
+    public GameObject textBox;  //the gameobject that holds the text.
+
+    public float textTypingSpeed = 1.0f; //how fast the type writing effect is going to be
+
+    public float dialogueLifetime = 5.0f; // the time it stays open before it close.
+    //create a list that holds string(text or comment)
+    public string[] dialogueTextChoices;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.N)) // test
+        {
+            BeginTalk(0);
+        }
+    }
+
+    void BeginTalk(int TextChoice)
+    {
+        //called when catkeeper starts talking.
+        textBox.SetActive(true);
+        string chosenDialogue = dialogueTextChoices[TextChoice];
+        text.text = chosenDialogue;
+
+        StopAllCoroutines();
+        StartCoroutine(TypeWritingEffect(chosenDialogue));
+
+    }
+
+    void CloseDialogue()
+    {
+        textBox.SetActive(false);
+    }
+
+    IEnumerator TypeWritingEffect(string dialogue)
+    {
+        text.text = "";
+        foreach (char c in dialogue)
+        {
+            text.text += c; // add 1 character
+            yield return new WaitForSeconds(textTypingSpeed);
+        }
+
+        yield return new WaitForSeconds(dialogueLifetime);
+        CloseDialogue();
+    }
+
+    //---- Triggers ---// 
+
+    //void DetectLowHealth
+
+    void DetectLowHPCat()
+    {
+        //Find all cat in - game.
+        //access their current HP and Max HP
+        //if any cat health is below 25%. BeginTalk(1)
     }
 }
